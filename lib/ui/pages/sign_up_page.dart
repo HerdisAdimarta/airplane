de044ea5f9e3cf59f438ac9d184c0015d3b7915f
HerdisAdimarta <herdies.adimarta@gmail.com>
2022-04-20 16:05:50 +0700

    slicing sign up input section 2

--- lib/ui/pages/sign_up_page.dart
+++ lib/ui/pages/sign_up_page.dart
@@ -20,7 +20,7 @@ class SignUpPage extends StatelessWidget {
     }
 
     Widget inputSection(){
-      Widget emailInput(){
+      Widget nameInput(){
         return Container(
           margin: EdgeInsets.only(bottom: 20),
           child: Column(
@@ -50,7 +50,133 @@ class SignUpPage extends StatelessWidget {
           ),
         );
       }
-
+      Widget emailInput(){
+        return Container(
+          margin: EdgeInsets.only(bottom: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('Email Address'),
+              SizedBox(
+                height: 6,
+              ),
+              TextFormField(
+                cursorColor: kBlackColor,
+                decoration: InputDecoration(
+                  hintText: 'Your email address',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(defaultRadius,
+                    )
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(defaultRadius),
+                    borderSide: BorderSide(
+                      color: kPrimaryColor,
+                    )
+                  )
+                ),
+              )
+            ],
+          ),
+        );
+      }
+      Widget passwordInput(){
+        return Container(
+          margin: EdgeInsets.only(bottom: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('Password'),
+              SizedBox(
+                height: 6,
+              ),
+              TextFormField(
+                cursorColor: kBlackColor,
+                obscureText: true,
+                decoration: InputDecoration(
+                  hintText: 'Your password',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(defaultRadius,
+                    )
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(defaultRadius),
+                    borderSide: BorderSide(
+                      color: kPrimaryColor,
+                    )
+                  )
+                ),
+              )
+            ],
+          ),
+        );
+      }
+      Widget hobyInput(){
+        return Container(
+          margin: EdgeInsets.only(bottom: 30),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('Hobby'),
+              SizedBox(
+                height: 6,
+              ),
+              TextFormField(
+                cursorColor: kBlackColor,
+                decoration: InputDecoration(
+                    hintText: 'Your hobby',
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(defaultRadius,
+                        )
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(defaultRadius),
+                        borderSide: BorderSide(
+                          color: kPrimaryColor,
+                        )
+                    )
+                ),
+              )
+            ],
+          ),
+        );
+      }
+      Widget submitButton(){
+        return Container(
+          width: double.infinity,
+          height: 55,
+          child: TextButton(
+            onPressed: () {},
+            style: TextButton.styleFrom(
+              backgroundColor: kPrimaryColor,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(defaultRadius)
+              )
+            ),
+            child: Text(
+              'Get Started',
+              style: whiteTextStyle.copyWith(
+                  fontSize: 18,
+                  fontWeight: medium
+              ),
+            ),
+          ),
+        );
+      }
+      Widget tacButton() {
+        return Container(
+          alignment: Alignment.center,
+          margin: EdgeInsets.only(top: 50, bottom: 73),
+          child: Text(
+            'Terms and Conditions',
+            style: greyTextStyle.copyWith(
+              fontSize: 16,
+              fontWeight: light,
+              decoration: TextDecoration.underline,
+            ),
+          ),
+        );
+      }
 
       return Container(
         margin: EdgeInsets.only(top: 30),
@@ -65,7 +191,12 @@ class SignUpPage extends StatelessWidget {
         ),
         child: Column(
           children: [
+            nameInput(),
             emailInput(),
+            passwordInput(),
+            hobyInput(),
+            submitButton(),
+            tacButton(),
           ],
         ),
       );
